2f5b3f260b6752fa8cb95efcdf7ba7ff9ba4ce81
svmadap <81061506+suchir-madap@users.noreply.github.com>
2024-03-21 11:54:27 -0400

    first changes

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import excercises from './assets/data/exercises.json'
+
 export default function App() {
-  return (
+    const excercise = excercises[0];
+    return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <View style = {styles.excerciseContainer}>
+        <Text style={styles.excerciseName}>{excercise.name}</Text>
+        <Text style = {styles.excerciseSubtitle}> {excercise.muscle.toUpperCase()} | {excercise.equipment.toUpperCase()}</Text>
+
+        </View>
+      
+    <StatusBar style="auto" />
     </View>
   );
 }
@@ -13,8 +21,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'gainsboro',
     justifyContent: 'center',
+    padding: 10,
   },
+  excerciseContainer: {
+    backgroundColor: '#fff',
+    padding: 10,
+    borderRadius: 10,
+    gap: 5, 
+  },
+  excerciseName: {
+    fontSize: 20,
+    fontWeight: '500',
+  },
+  excerciseSubtitle: {
+    color: 'dimgray',
+  }
 });
